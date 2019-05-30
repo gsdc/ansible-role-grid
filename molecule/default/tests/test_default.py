b13875dcf3789aa90594a75329043adc759eec37
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2019-05-30 19:15:59 +0200

    Define meaningful tests ...

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,13 +2,30 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import pytest
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
+@pytest.mark.parametrize('name', [
+    '/etc/grid-security/hostcert.pem',
+    '/etc/vomses/cms-lcg-voms2.cern.ch',
+    '/etc/grid-security/vomsdir/cms/lcg-voms2.cern.ch.lsc',
+    '/etc/yum.repos.d/UMD-4-updates.repo'
+])
+def test_files(host,name):
     f = host.file('/etc/hosts')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+
+@pytest.mark.parametrize('name,version', [
+    ('ca-policy-lcg', '1.'),
+    ('fetch-crl', '3.0.'),
+])
+def test_packages(host,name,version):
+    pkg = host.packages('/etc/hosts')
+
+    assert pkg.is_installed
+    assert pkg.version.startswith(version)
